e5b58f38da87d49b332f4c92e1f7d6ea38cc9b01
Mohamed Bakayoko <mowamedbakus@gmail.com>
2021-05-25 14:13:06 +0000

    add gorm and connect to mysql DB

--- go.mod
+++ go.mod
@@ -6,4 +6,6 @@ require (
 	github.com/gofiber/fiber/v2 v2.10.0 // indirect
 	github.com/valyala/fasthttp v1.25.0 // indirect
 	golang.org/x/sys v0.0.0-20210521203332-0cec03c779c1 // indirect
+	gorm.io/driver/mysql v1.1.0 // indirect
+	gorm.io/gorm v1.21.10 // indirect
 )
--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"os"
+)
 
 func main() {
+	db, err := gorm.Open(mysql.Open(os.Getenv("GO_ADMIN_DB")), &gorm.Config{})
+
+	if err != nil {
+		panic("Could not connect to the database")
+	}
+	fmt.Println(db)
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
