cd79bdbf15a216799b2d83db249dd2dfc4d4a9f1
Mohamed Bakayoko <mowamedbakus@gmail.com>
2021-05-30 20:14:28 +0000

    add logout

--- controllers/authController.go
+++ controllers/authController.go
@@ -116,3 +116,18 @@ func User(c *fiber.Ctx) error {
 
 	return c.JSON(user)
 }
+
+func Logout(c *fiber.Ctx) error {
+	cookie := fiber.Cookie{
+		Name:     "jwt",
+		Value:    "",
+		Expires:  time.Now().Add(-time.Hour),
+		HTTPOnly: true,
+	}
+
+	c.Cookie(&cookie)
+
+	return c.JSON(fiber.Map{
+		"message": "success",
+	})
+}
--- routes/routes.go
+++ routes/routes.go
@@ -9,5 +9,6 @@ func Setup(app *fiber.App) {
 
 	app.Post("/api/register", controllers.Register)
 	app.Post("/api/login", controllers.Login)
+	app.Post("/api/logout", controllers.Logout)
 	app.Get("/api/user", controllers.User)
 }
