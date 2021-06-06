1de17fa70e2e7d166889ac398fa9ac25b550f44a
Mohamed Bakayoko <mowamedbakus@gmail.com>
2021-06-06 22:27:45 +0000

    export orders as csv

--- controllers/orderController.go
+++ controllers/orderController.go
@@ -1,9 +1,11 @@
 package controllers
 
 import (
+	"encoding/csv"
 	"github.com/gofiber/fiber/v2"
 	"github.com/mowamed/go-admin/database"
 	"github.com/mowamed/go-admin/models"
+	"os"
 	"strconv"
 )
 
@@ -11,4 +13,70 @@ func AllOrders(c *fiber.Ctx) error {
 	page, _ := strconv.Atoi(c.Query("page", "1"))
 
 	return c.JSON(models.Paginate(database.DB, &models.Order{}, page))
+}
+
+func Export(c *fiber.Ctx) error {
+	filePath := "./csv/orders.csv"
+
+	if err := CreateFile(filePath); err != nil {
+		return err
+	}
+
+	return c.Download(filePath)
+}
+
+func CreateFile(filePath string) error {
+	file, err := os.Create(filePath)
+
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+
+	defer writer.Flush()
+
+	var orders []models.Order
+
+	database.DB.Preload("OrderItems").Find(&orders)
+
+	writer.Write([]string{
+		"Id", "Name", "Email", "Product Title", "Price", "Quantity",
+	})
+
+	for _, order := range orders {
+		data := []string{
+			strconv.Itoa(int(order.Id)),
+			order.FirstName + " " + order.LastName,
+			order.Email,
+			"",
+			"",
+			"",
+		}
+
+		if err := writer.Write(data); err != nil {
+			return err
+		}
+
+		for _, orderItem := range order.OrderItems {
+			data := []string{
+				"",
+				"",
+				"",
+				orderItem.ProductTitle,
+				strconv.Itoa(int(orderItem.Price)),
+				strconv.Itoa(orderItem.Quantity),
+			}
+
+			if err := writer.Write(data); err != nil {
+				return err
+			}
+		}
+
+	}
+
+	return nil
+
 }
--- routes/routes.go
+++ routes/routes.go
@@ -43,5 +43,6 @@ func Setup(app *fiber.App) {
 	app.Static("/api/uploads", "./uploads")
 
 	app.Get("/api/orders", controllers.AllOrders)
+	app.Post("/api/export", controllers.Export)
 
 }
