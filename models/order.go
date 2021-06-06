e359f455b23016195bb63d3f4b0dff168c856351
Mohamed Bakayoko <mowamedbakus@gmail.com>
2021-06-06 16:13:00 +0000

    get order total amount

--- models/order.go
+++ models/order.go
@@ -4,8 +4,10 @@ import "gorm.io/gorm"
 
 type Order struct {
 	Id         uint        `json:"id"`
-	FirstName  string      `json:"first_name"`
-	LastName   string      `json:"last_name"`
+	FirstName  string      `json:"-"`
+	LastName   string      `json:"-"`
+	Name       string      `json:"name" gorm:"-"`
+	Total      float32     `json:"total" gorm:"-"`
 	Email      string      `json:"email"`
 	UpdatedAt  string      `json:"updated_at"`
 	CreatedAt  string      `json:"created_at"`
@@ -17,7 +19,7 @@ type OrderItem struct {
 	OrderId      uint
 	ProductTitle string
 	Price        float32
-	Quantity     string
+	Quantity     int
 }
 
 func (order *Order) Count(db *gorm.DB) int64 {
@@ -29,5 +31,16 @@ func (order *Order) Count(db *gorm.DB) int64 {
 func (order *Order) Take(db *gorm.DB, limit int, offset int) interface{} {
 	var orders []Order
 	db.Preload("OrderItems").Offset(offset).Limit(limit).Find(&orders)
+
+	for i, _ := range orders {
+		var total float32 = 0
+
+		for _, orderItem := range orders[i].OrderItems {
+			total += orderItem.Price * float32(orderItem.Quantity)
+		}
+
+		orders[i].Name = orders[i].FirstName + " " + orders[i].LastName
+		orders[i].Total = total
+	}
 	return orders
 }
